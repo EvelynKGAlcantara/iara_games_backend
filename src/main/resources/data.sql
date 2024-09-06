74e4ca0e86f23d17b1a10819108ed64c274dc546
Evelyn Karoline Gusson Alcântara <evelynkgalcantara@gmail.com>
2024-09-05 22:15:38 -0300

    atualizacoes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_categoria (nome)
+VALUES ('RPG');
+INSERT INTO tb_categoria (nome)
+VALUES ('Corrida');
+INSERT INTO tb_categoria (nome)
+VALUES ('Aventura');
+
+INSERT INTO tb_fabricante (nome)
+VALUES ('Ubisoft');
+INSERT INTO tb_fabricante (nome)
+VALUES ('Epic games');
+INSERT INTO tb_fabricante (nome)
+VALUES ('FromSoftware');
